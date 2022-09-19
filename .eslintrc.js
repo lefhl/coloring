7782e92dc769e5a8724363ed5a44938232cb2d47
edik <gizbreht.e@yandex.ru>
2022-09-19 10:46:48 +0300

    черновая версия сайта

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:json/recommended",
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  globals: {
+    Foundation: true,
+  },
+};
